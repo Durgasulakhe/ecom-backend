935b9fff252f04c922aed9fa326fcc2a617dbac8
Durgasulakhe <durgasulakhe2004@klebcahubli.in>
2025-03-19 11:34:46 +0530

    final change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const {User} = require('./model/User');
 const mongoose = require('mongoose');
 const cors= require('cors');
 const morgan= require('morgan');
-const bcrypt= require('bcrypt');
+const bcrypt= require('bcryptjs');
 const jwt= require('jsonwebtoken');
 const {Product} = require('./model/Product');
 const {Cart} =require('./model/cart');
@@ -13,9 +13,11 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
+//PpDQYVZgOt7vSaDv
 
+let MONGODB_URL ="mongodb+srv://durgasulakhe2004:PpDQYVZgOt7vSaDv@cluster0.len43.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleproject')
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log("DB is connected")
 }).catch((err)=>{
